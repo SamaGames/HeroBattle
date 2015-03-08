af53423594f3dcde1487b013baf7b5737a217c78
Amaury Carrade <amaury@carrade.eu>
2015-03-08 20:34:45 +0100

    Fixed message displayed when the player quits the game with the door.

--- src/main/java/net/lnfinity/HeroBattle/Listeners/MasterListener.java
+++ src/main/java/net/lnfinity/HeroBattle/Listeners/MasterListener.java
@@ -128,7 +128,7 @@ public class MasterListener implements Listener {
 	public void onPlayerInteract(PlayerInteractEvent e) {
 		if (plugin.getGame().getStatus() != Status.InGame
 				&& e.hasItem() && e.getItem().equals(plugin.getCoherenceMachine().getLeaveItem())) {
-			e.getPlayer().kickPlayer("Bye bye");
+			e.getPlayer().kickPlayer("");
 		}
 	}
 }
