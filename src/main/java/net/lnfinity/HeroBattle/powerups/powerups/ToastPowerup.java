d158bc8eadba045eafd1a2c7039d0ce581235ba7
Amaury Carrade <amaury@carrade.eu>
2015-04-09 18:31:58 +0200

    Updated the toast powerup.

--- src/main/java/net/lnfinity/HeroBattle/powerups/powerups/ToastPowerup.java
+++ src/main/java/net/lnfinity/HeroBattle/powerups/powerups/ToastPowerup.java
@@ -1,18 +1,28 @@
 package net.lnfinity.HeroBattle.powerups.powerups;
 
 import net.lnfinity.HeroBattle.powerups.NegativePowerup;
+import net.lnfinity.HeroBattle.utils.Utils;
 import net.md_5.bungee.api.ChatColor;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.util.Vector;
+
+import java.util.Random;
 
 
 public class ToastPowerup implements NegativePowerup {
 
 	@Override
 	public void onPickup(Player player, ItemStack pickupItem) {
-		Bukkit.getServer().broadcastMessage(ChatColor.DARK_PURPLE + "TOAST... JUMP!");
+
+		player.setVelocity(player.getVelocity().add(new Vector(0, (new Random()).nextDouble() + 3, 0)));
+		player.playSound(player.getLocation(), Sound.ENDERMAN_TELEPORT, 1.8f, 1);
+
+		player.sendMessage(ChatColor.RED + "Wooosh !");
+
 	}
 
 	@Override
