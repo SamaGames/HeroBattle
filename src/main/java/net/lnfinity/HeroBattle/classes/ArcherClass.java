235021fa82f4a5676ce197848d097956d3f566c0
Amaury Carrade <amaury@carrade.eu>
2015-04-13 00:38:59 +0200

    Changed the cooldown of the Archer class and reorganized the tools.

--- src/main/java/net/lnfinity/HeroBattle/classes/ArcherClass.java
+++ src/main/java/net/lnfinity/HeroBattle/classes/ArcherClass.java
@@ -22,8 +22,8 @@ public class ArcherClass extends PlayerClass {
 		super(plugin);
 
 		addTool(new SwordVariant3Tool(p));
+		addTool(new ArrowsTool(p, 12 - arg1, 3 + arg2));
 		addTool(new SpeedTool(p, 20 - arg1 * 2, 5 + arg2));
-		addTool(new ArrowsTool(p, 20 - arg1, 3 + arg2));
 	}
 
 	@Override
