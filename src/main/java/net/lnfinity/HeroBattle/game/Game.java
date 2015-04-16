77184f3ecaf78b78a87d6bb5a88d7808e95d8ccc
Amaury Carrade <amaury@carrade.eu>
2015-04-16 22:56:27 +0200

    Fixed separator incorrectly displayed in the leaderboard.

--- src/main/java/net/lnfinity/HeroBattle/game/Game.java
+++ src/main/java/net/lnfinity/HeroBattle/game/Game.java
@@ -529,11 +529,11 @@ public class Game implements GameArena {
 				Bukkit.broadcastMessage(ChatColor.GOLD + "----------------------------------------------------");
 				Bukkit.broadcastMessage(ChatColor.GOLD + "                      Classement des Kills          ");
 				Bukkit.broadcastMessage(ChatColor.GOLD + "                                                    ");
-				Bukkit.broadcastMessage(ChatColor.YELLOW + " " + topsKills[0] + ChatColor.DARK_GRAY + " ⋅ " + ChatColor.GRAY +  topsKills[1] + ChatColor.DARK_GRAY + " ⋅ " + ChatColor.GOLD + topsKills[2]);
+				Bukkit.broadcastMessage(ChatColor.YELLOW + " " + topsKills[0] + ChatColor.DARK_GRAY + (!topsKills[1].isEmpty() ? " ⋅ " : "") + ChatColor.GRAY +  topsKills[1] + ChatColor.DARK_GRAY + (!topsKills[2].isEmpty() ? " ⋅ " : "") + ChatColor.GOLD + topsKills[2]);
 				Bukkit.broadcastMessage(ChatColor.GOLD + "                                                    ");
 				Bukkit.broadcastMessage(ChatColor.GOLD + "                  Classement des dégâts infligés    ");
 				Bukkit.broadcastMessage(ChatColor.GOLD + "                                                    ");
-				Bukkit.broadcastMessage(ChatColor.YELLOW + " " + topsPercentages[0] + ChatColor.DARK_GRAY + " ⋅ " + ChatColor.GRAY +  topsPercentages[1] + ChatColor.DARK_GRAY + " ⋅ " + ChatColor.GOLD + topsPercentages[2]);
+				Bukkit.broadcastMessage(ChatColor.YELLOW + " " + topsPercentages[0] + ChatColor.DARK_GRAY + (!topsPercentages[1].isEmpty() ? " ⋅ " : "") + ChatColor.GRAY +  topsPercentages[1] + ChatColor.DARK_GRAY + (!topsPercentages[2].isEmpty() ? " ⋅ " : "") + ChatColor.GOLD + topsPercentages[2]);
 				Bukkit.broadcastMessage(ChatColor.GOLD + "                                                    ");
 				Bukkit.broadcastMessage(ChatColor.GOLD + "----------------------------------------------------");
 
