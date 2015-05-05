ae0ae4de52ac5e68ec60d988c9c52dfd274b42b6
Florian Cassayre <florian.cassayre@gmail.com>
2015-05-05 23:32:19 +0200

    Fix boutique

--- src/main/java/net/lnfinity/HeroBattle/classes/ClassManager.java
+++ src/main/java/net/lnfinity/HeroBattle/classes/ClassManager.java
@@ -112,7 +112,7 @@ public class ClassManager {
 				public void run() {
 					if (MasterBundle.isDbEnabled) {
 						String data = FastJedis.get(prefix + className + "." + has + sufix);
-						if((data != null && (data.equals("1")) || className.equals("brute") || className.equals("guerrier") || className.equals("archer") || className.equals("mage"))) {
+						if((data != null && data.equals("1")) || className.equals("brute") || className.equals("guerrier") || className.equals("archer") || className.equals("mage")) {
 							try {
 							String A = FastJedis.get(prefix + className + "." + cooldown + sufix);
 							if(A == null || A.equals("")) {
