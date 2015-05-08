acabae76198b633af28c09e03bfd39bc1cfd284e
Florian Cassayre <florian.cassayre@gmail.com>
2015-05-08 17:03:37 +0200

    Cas où le joueur se connecte au dernier moment (temporaire)

--- src/main/java/net/lnfinity/HeroBattle/game/Game.java
+++ src/main/java/net/lnfinity/HeroBattle/game/Game.java
@@ -2,6 +2,7 @@ package net.lnfinity.HeroBattle.game;
 
 import net.lnfinity.HeroBattle.HeroBattle;
 import net.lnfinity.HeroBattle.classes.PlayerClass;
+import net.lnfinity.HeroBattle.classes.displayers.BruteClass;
 import net.lnfinity.HeroBattle.tools.PlayerTool;
 import net.lnfinity.HeroBattle.utils.ActionBar;
 import net.lnfinity.HeroBattle.utils.Utils;
@@ -14,6 +15,7 @@ import net.samagames.utils.GlowEffect;
 import net.samagames.utils.Titles;
 import net.zyuiop.MasterBundle.MasterBundle;
 import net.zyuiop.statsapi.StatsApi;
+
 import org.bukkit.*;
 import org.bukkit.block.Block;
 import org.bukkit.craftbukkit.v1_8_R1.entity.CraftPlayer;
@@ -304,7 +306,13 @@ public class Game implements GameArena {
 
 		int r = rnd.nextInt(gamePlayer.getAvaibleClasses().size());
 		int i = 0;
-
+		
+		// Avoid game crashing
+		if(gamePlayer.getAvaibleClasses() == null || gamePlayer.getAvaibleClasses().isEmpty()) {
+			gamePlayer.setPlayerClass(new BruteClass(p, 0, 0, 0));
+			return;
+		}
+		
 		for (PlayerClass classe : gamePlayer.getAvaibleClasses()) {
 			if (i == r) {
 				gamePlayer.setPlayerClass(classe);
