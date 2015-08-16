77b82a704f1996990d779446216b7f3eed36ee64
Amaury Carrade <amaury@carrade.eu>
2015-08-16 15:02:22 +0200

    Fixed nether star acting as a class selector even after the beginning of the game.
    * BUG: The nether star opens the class selector GUI even when the game is started.

--- src/main/java/net/lnfinity/HeroBattle/listeners/ClassSelectorListener.java
+++ src/main/java/net/lnfinity/HeroBattle/listeners/ClassSelectorListener.java
@@ -3,6 +3,7 @@ package net.lnfinity.HeroBattle.listeners;
 import net.lnfinity.HeroBattle.*;
 import net.lnfinity.HeroBattle.gui.*;
 import net.lnfinity.HeroBattle.gui.core.*;
+import net.samagames.gameapi.json.*;
 import org.bukkit.*;
 import org.bukkit.event.*;
 import org.bukkit.event.player.*;
@@ -15,13 +16,18 @@ public class ClassSelectorListener implements Listener
 	{
 		if (e.hasItem())
 		{
-			if (e.getItem().getType() == Material.NETHER_STAR)
+			Status gameStatus = HeroBattle.getInstance().getGame().getStatus();
+
+			if(gameStatus == Status.Available || gameStatus == Status.Starting)
 			{
-				Gui.open(e.getPlayer(), new ClassSelectorGui());
-			}
-			else if (e.getItem().getType() == Material.BOOK)
-			{
-				HeroBattle.getInstance().getTutorialDisplayer().start(e.getPlayer().getUniqueId());
+				if (e.getItem().getType() == Material.NETHER_STAR)
+				{
+					Gui.open(e.getPlayer(), new ClassSelectorGui());
+				}
+				else if (e.getItem().getType() == Material.BOOK)
+				{
+					HeroBattle.getInstance().getTutorialDisplayer().start(e.getPlayer().getUniqueId());
+				}
 			}
 		}
 	}
