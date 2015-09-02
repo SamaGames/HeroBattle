b48d4c6307413ebb923ae2efc7e21a237a99c470
Amaury Carrade <amaury@carrade.eu>
2015-09-02 15:24:49 +0200

    Fixed non-migrated file.
    * BUG: removed reference from an old class (GamePlayer, now using HeroBattlePlayer) in a tool displayer (Fruit tool).

--- src/main/java/net/lnfinity/HeroBattle/tools/displayers/FruitTool.java
+++ src/main/java/net/lnfinity/HeroBattle/tools/displayers/FruitTool.java
@@ -1,19 +1,16 @@
 package net.lnfinity.HeroBattle.tools.displayers;
 
-import java.util.List;
-
-import net.lnfinity.HeroBattle.HeroBattle;
-import net.lnfinity.HeroBattle.tools.PlayerTool;
-import net.lnfinity.HeroBattle.utils.ItemCooldown;
-import net.lnfinity.HeroBattle.utils.ToolsUtils;
-import net.lnfinity.HeroBattle.utils.Utils;
+import net.lnfinity.HeroBattle.*;
+import net.lnfinity.HeroBattle.game.*;
+import net.lnfinity.HeroBattle.tools.*;
+import net.lnfinity.HeroBattle.utils.*;
 import net.md_5.bungee.api.ChatColor;
+import org.bukkit.*;
+import org.bukkit.entity.*;
+import org.bukkit.event.player.*;
+import org.bukkit.inventory.*;
 
-import org.bukkit.Material;
-import org.bukkit.Sound;
-import org.bukkit.entity.Player;
-import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.inventory.ItemStack;
+import java.util.*;
 
 public class FruitTool extends PlayerTool {
 	
@@ -54,15 +51,10 @@ public class FruitTool extends PlayerTool {
 			new ItemCooldown(p, player, this, COOLDOWN);
 			player.playSound(player.getLocation(), Sound.BURP, 1, 1);
 			
-			GamePlayer gamePlayer = p.getGamePlayer(player);
+			HeroBattlePlayer gamePlayer = p.getGamePlayer(player);
 			gamePlayer.addRemainingDoubleDamages(DURATION);
 		} else {
 			player.sendMessage(ChatColor.RED + "Vous êtes trop fatigué pour réutiliser ça maintenant");
 		}
 	}
-
-	@Override
-	public void onLeftClick(Player player, ItemStack tool, PlayerInteractEvent event) {
-	}
-
 }
